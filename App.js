0ab1fd11545fb3b9153db2a810af8a12dd51ee0b
JustinMitchell <mitchelljm04@gmail.com>
2023-04-18 11:53:35 -0400

    Setup

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableWithoutFeedback, Image, SafeAreaView } from 'react-native';
+import { useDeviceOrientation } from '@react-native-community/hooks';
 
 export default function App() {
+
+  const {landscape} = useDeviceOrientation()
+
+  const handlePress = () => {
+    console.log('Pressed Text')
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text onPress={handlePress}>This is a new app </Text>
+      <View style={{  backgroundColor: 'blue',
+  width: '100%',
+  height: landscape ? '100%' : '50%'}}>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'whitesmoke',
+    // justifyContent: 'center',
+    // alignItems: 'center',
+    color: 'white'
   },
 });
